77010cbad855edf7ed98f1aa58d82551425c0a82
Cristian <Boiam>
2020-06-15 17:47:33 -0300

    Init

---
+++ src/WeatherForecast.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherForecast.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
---
+++ src/WeatherForecast.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WeatherForecast.Core
+{
+    public class Class1
+    {
+    }
+}
