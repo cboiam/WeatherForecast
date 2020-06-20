5e8ec6eae19015d5b684fed7202cbfcaa375af3e
cristian <cristianboiam@hotmail.com>
2020-06-20 15:19:53 -0300

    Unused constructors

--- src/WeatherForecast.Core/BusinessException.cs
+++ src/WeatherForecast.Core/BusinessException.cs
@@ -4,12 +4,7 @@ namespace WeatherForecast.Core
 {
     public class BusinessException : Exception
     {
-        public BusinessException() { }
-
         public BusinessException(string message) 
             : base(message) { }
-
-        public BusinessException(string message, Exception innerException) 
-            : base(message, innerException) { }
     }
 }
